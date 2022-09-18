909ac50e804276eef460abd3b21407eb304f6bfe
PedroP54 <paragonr54@gmail.com>
2022-09-18 18:57:04 -0500

    Add project files.

---
+++ PracticeTest/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int val1;
+	{
+		cout << "Enter value #1:\t";
+	    cin >> val1;
+	}
+	int val2;
+	{
+		cout << "Enter value #2:\t";
+		cin >> val2;
+	}
+	int val3;
+	{
+		cout << "Enter value #3:\t";
+		cin >> val3;
+	}
+	int val4;
+	{
+		cout << "Enter value #4:\t";
+		cin >> val4;
+	}
+	cout << endl << "Result of the mean and population standar deviation: \t";
+}
